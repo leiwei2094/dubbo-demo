1dd47d8c32b48a4d0559e9b856b65258ed98200e
Leo-Lei <leiwei2094@gmail.com>
2016-09-24 15:36:52 +0800

    Delete files.

--- dubbox-hessian-demo/api/src/main/java/com/leolei/dubbodemo/api/IHelloService.java
+++
@@ -1,7 +0,0 @@
-package com.leolei.dubbodemo.api;
-
-import java.util.List;
-
-public interface IHelloService {
-    String sayHello(String name);
-}
--- dubbox-hessian-demo/api/src/main/java/com/leolei/dubbodemo/api/IUserService.java
+++
@@ -1,6 +0,0 @@
-package com.leolei.dubbodemo.api;
-
-public interface IUserService {
-
-    User getUser(String name);
-}
